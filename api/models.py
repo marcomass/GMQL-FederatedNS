96c5720f36cc152f015eb3017f8d2366f824f4f5
Andrea Gulino <andrea.gulino@polimi.it>
2018-09-03 18:14:40 +0200

    Fix import BaseUser

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.utils import timezone
 
-from django.contrib.auth.base_user import BaseUserManager
+
 from django.db import models
 
 from django.conf import settings
@@ -11,7 +11,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -19,7 +19,7 @@ def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
 
-class MyUserManager(BaseUserManager):
+class MyUserManager(UserManager):
     def create_user(self, name, namespace, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
