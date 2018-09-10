c3194817983518b1a826966aa1aa0eeebe857464
Andrea Gulino <andrea.gulino@polimi.it>
2018-09-10 09:39:40 +0200

    Add Form Parser

--- NameServer/settings.py
+++ NameServer/settings.py
@@ -53,6 +53,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework_xml.parsers.XMLParser',
         'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework_xml.renderers.XMLRenderer',
